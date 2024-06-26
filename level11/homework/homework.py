8628b2d5633c168bd01fa2ecbce08956ccbe6962
Andria11rt <andiaandria21@gmail.com>
2024-06-26 20:02:21 +0400

    second commit

---
+++ level11/homework/homework.py
@@ -0,0 +1,58 @@
+#1) გააკეთეთ 30 მაგალითი შედარებით ოპერატორებზე.
+
+print(100 > 5)
+print(10 < 66)
+print(133 > 5)
+print(104 > 5)
+print(103 > 5)
+print(101 > 5)
+print(106 > 5)
+print(107 > 5)
+print(108 > 5)
+print(109 > 5)
+print(33 > 3)
+
+print(1020 > 5)
+print(1023 > 5)
+print(1033 > 5)
+print(10234 > 5)
+print(10123 > 5)
+print(10123 > 5)
+print(10345 > 5)
+print(1067 > 5)
+print(1086 > 5)
+print(10234 > 5)
+print(3323 > 3)
+
+print(10 < 5)
+print(10 < 5)
+print(10 < 5)
+print(10 < 5)
+print(10 < 5)
+print(10 < 5)
+print(10 < 5)
+print(10 < 5)
+print(10 < 5)
+print(10 < 5)
+print(33 < 3)
+
+#2) დაწერეთ პროგრამა, რომელიც იღებს ორი ადამიანის ასაკს და განსაზღვრავს ვინ არის უფროსი.
+
+age1 = input("First user age: ")
+age2 = input("Second user age: ")
+print(age1 < age2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
