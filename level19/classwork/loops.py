edb725d942d5c4d9b85449eb05b160b93e948274
Andria11rt <andiaandria21@gmail.com>
2024-07-24 19:55:02 +0400

    second commit

---
+++ level19/classwork/loops.py
@@ -0,0 +1,36 @@
+
+
+# 
+
+#for i in range(1, 15):
+    #print("3 *", i, "=", 3 * i)
+
+
+#x = 1
+
+#x = x + 1
+
+#x += 10
+
+#print(x)
+
+# program: how to sum namber
+
+#total = 0
+#for i in range(1, 20):
+    #total += i # - ეს უდრის total = total + i
+#print("sum", total)
+
+# reverse countdown
+
+#for i in range(10, 1, -1):
+    #print(i)
+
+
+
+
+
+#count = 1
+#while count <= 10: # False
+    #print("hello")
+    #count += 1
