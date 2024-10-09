d74c14ac23fbc641a9d3aeacc35d195811257517
Andria11rt <andiaandria21@gmail.com>
2024-10-09 20:55:20 +0400

    second commit

---
+++ level41/homework/home.py
@@ -0,0 +1,11 @@
+# Task 2 String Formating
+#  ?
+
+# Task 3
+# Create a list named words that contains the following strings: "apple", "banana", "cherry".
+# Use the join() function to combine these words into a single string, 
+# separated by a comma and a space ", ". Store the result in a variable named fruit_string and print it.
+
+# ?
+
+# 
